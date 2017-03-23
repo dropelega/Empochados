a45670a5c90fc4eace2ea9a2f1423a2b046dbfae
dropelega <pdredondo@gmail.com>
2017-03-23 09:36:11 +0100

    Un rectangulo por numero
    Deteccion de numero perfecta

--- Code/Empochados/main.py
+++ Code/Empochados/main.py
@@ -1,10 +1,55 @@
 import cv2
 import numpy as np
 import comparadorColores as cc
-image = cv2.imread('/home/dropelega/Empotrados/Empochados/Test/rey-de-oros.jpg')
+import InputImages as ii
+path='/home/miguel/PycharmProjects/Empochados-master/cartas/'
+cargar= ii.InputImages(path)
+images=cargar.images()
+for i in images:
+    image = cv2.imread(path+i)
+    imageStandard=cv2.resize(image,(493,755))
+    image2= image[40:120,40:120]
+    image3= cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+    mini= image3.copy()
+    mini2= cv2.resize(mini,(250,250))
+    mser = cv2.MSER_create(_delta=8  )
+    regions= mser.detectRegions(mini2,None)
+    rect = [cv2.boundingRect(p) for p in regions]
+    rectbuenos=[]
+    for r in rect:
+        if((r[2])<0.66*(r[3])):
+            if(r[2]<1.4*image2.shape[0] and r[2]>0.5*image2.shape[0]):
+                if (r[3]<1.4*image2.shape[1] and r[3]>0.5*image2.shape[1]):
+                    rectbuenos.append(r)
+    rectbuenos.sort()
+    print(rectbuenos)
+    rectOptimos=[]
+    rectOptimos.append(rectbuenos[0])
+
+    for rb in rectbuenos:
+        changed = False
+        posicionOptimos=0
+        print("rb",rb)
+        for rt in rectOptimos:
+            if(rb[1]>=rt[1]+(rt[3]/5)):
+                changed=True
+                break
+            if((rt[0]<=rb[0]<=rt[2]+rt[0])and(rt[1]<=rb[1]<=rt[3]+rt[1])):
+                rectOptimos[posicionOptimos]=rb
+                changed=True
+                break
+            posicionOptimos+=1
+        if(not changed):
+            rectOptimos.append(rb)
+    print(len(rectOptimos))
+    for r in rectOptimos:
+        cv2.rectangle(mini2, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (0, 255, 0))
+    cv2.imshow('img', mini2)
+    cv2.waitKey(0)
+
 #cv2.imshow('P',image)
 #cv2.waitKey()
- 
+
 # define the list of range colors
 rcolors = [
     ([0, 0, 100], [110 , 110, 255]), #red
@@ -26,12 +71,13 @@ for (lower, upper) in rcolors:
                 if color[0]!=0 or color[1]!=0 or color[2]!=0:
                     colorsPercentaje[i]+=1
            
-        cv2.imshow("Colores", np.hstack([image,output]))
-        cv2.waitKey(0) 
+        #cv2.imshow("Colores", np.hstack([image,output]))
+        #cv2.waitKey(0)
         i+=1
 comparador = cc.comparadorColores(colorsPercentaje)
-print colorsPercentaje
-print comparador.palo()
+print (colorsPercentaje)
+print (comparador.palo())
+
         
 
         
