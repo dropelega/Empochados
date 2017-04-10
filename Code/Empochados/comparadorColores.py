9f723c3697af917c12db258c0bb324bd627097de
miguelr95 <miguelroldanmachado@gmail.com>
2017-04-10 18:30:47 +0200

    Arduino terminado, integración python almost done

--- Code/Empochados/comparadorColores.py
+++ Code/Empochados/comparadorColores.py
@@ -17,12 +17,12 @@ class comparadorColores:
             i+=1
         #Decidimos a que palo pertenece la carta en caso de las "proporciones"
         if proporcion[1] ==1:
-            return "Espadas"
+            return "e"
         if proporcion[2] == 1 and proporcion[0]>0.2 and proporcion[3]<0.56:
-            return "Oros" 
+            return "o"
         if proporcion[0]==1 and proporcion[2]>0.2 and proporcion[3]<0.56:
-            return "Copas"
-        return "Bastos"
+            return "c"
+        return "b"
 
     def paloFiguras(self):
         #Obtenemos el numero de pixeles del color de mayor cantidad en la imagen
@@ -36,12 +36,12 @@ class comparadorColores:
             proporcion[i] = color / mayor
             i += 1
         if proporcion[1]==1:
-            return "Espadas"
+            return "e"
         if proporcion[2] == 1 and proporcion[0] < 0.5 and proporcion[3] < 0.85:
-            return "Oros"
+            return "o"
         if proporcion[0]==1 and proporcion[1]>0.4 and proporcion[3]<0.85:
-            return "Copas"
-        return "Bastos"
+            return "c"
+        return "b"
         
     def paloAs(self):
         # Obtenemos el numero de pixeles del color de mayor cantidad en la imagen
@@ -56,11 +56,11 @@ class comparadorColores:
             i += 1
         if proporcion[0] == 1:
             if proporcion[1]>0.7:
-                return "Espadas"
+                return "e"
             else:
-                return "Bastos"
+                return "b"
         if proporcion[2] == 1:
             if proporcion[1]>0.6:
-                return "Copas"
+                return "c"
             else:
-                return "Oros"
+                return "o"
--- Code/Empochados/main.py
+++ Code/Empochados/main.py
@@ -3,55 +3,54 @@ import numpy as np
 import comparadorColores as cc
 import InputImages as ii
 import detectorNumeros as dn
+import serial
 path='/home/miguel/Empochados/cartas/'
 cargar= ii.InputImages(path)
 images=cargar.images()
-
+pathcompleto=path+"11_de_oros.jpeg"
 #cv2.imshow('P',image)
 #cv2.waitKey()
 # define the list of range colors
 
-for images1 in images:
-    pathcompleto = path + images1
-    print(images1)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+detector = dn.detectorNumeros(pathcompleto)
+numeroRectangulos,ratio = detector.deteccionNumero()
+rcolors = [
+    ([0, 0, 100], [110 , 110, 255]), #red
+    ([86, 31, 31], [255, 210, 170]), #blue
+    ([25, 160, 120], [140, 235, 245]), #yellow
+    ([60, 110, 60], [130, 255,230]) #green
+]
+colorsPercentaje = [0,0,0,0]
+while(1):
+    i = 0
     image = cv2.imread(pathcompleto)
-    detector = dn.detectorNumeros(pathcompleto)
-    numeroRectangulos,ratio = detector.deteccionNumero()
-    rcolors = [
-        ([0, 0, 100], [110 , 110, 255]), #red
-        ([86, 31, 31], [255, 210, 170]), #blue
-        ([25, 160, 120], [140, 235, 245]), #yellow
-        ([60, 110, 60], [130, 255,230]) #green
-    ]
-    i=0
-    colorsPercentaje = [0,0,0,0]
-    if(numeroRectangulos>1):
-        shape=image.shape
-        image=image[0:int(shape[0]/4),0:int(shape[1]/2.2)]
-    for (lower, upper) in rcolors:
-            lower = np.array(lower, dtype = "uint8")
-            upper = np.array(upper, dtype= "uint8")
+    if(ser.read()=="p"):
+        if(numeroRectangulos>1):
+            shape=image.shape
+            image=image[0:int(shape[0]/4),0:int(shape[1]/2.2)]
+        for (lower, upper) in rcolors:
+                lower = np.array(lower, dtype = "uint8")
+                upper = np.array(upper, dtype= "uint8")
 
-            mask = cv2.inRange(image, lower, upper)
-            output = cv2.bitwise_and(image, image, mask=mask)
-            for column in output:
-                for color in column:
+                mask = cv2.inRange(image, lower, upper)
+                output = cv2.bitwise_and(image, image, mask=mask)
+                for column in output:
+                    for color in column:
 
-                    if color[0]!=0 or color[1]!=0 or color[2]!=0:
-                        colorsPercentaje[i]+=1
-
-            #cv2.imshow("Colores", np.hstack([image,output]))
-            #cv2.waitKey(0)
-            i+=1
-    comparador = cc.comparadorColores(colorsPercentaje)
-    if(numeroRectangulos==1):
-        if(ratio<0.45):
-            print(comparador.paloAs())
+                        if color[0]!=0 or color[1]!=0 or color[2]!=0:
+                            colorsPercentaje[i]+=1
+                #cv2.imshow("Colores", np.hstack([image,output]))
+                #cv2.waitKey(0)
+                i+=1
+        comparador = cc.comparadorColores(colorsPercentaje)
+        if(numeroRectangulos==1):
+            if(ratio<0.45):
+                ser.write(comparador.paloAs())
+            else:
+                ser.write(comparador.paloNormales())
         else:
-            print (comparador.paloNormales())
-    else:
 
-       print(comparador.paloFiguras())
-    print("")
+           ser.write(comparador.paloFiguras())
 
-        
+
